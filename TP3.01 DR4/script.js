2598518c621932bdd4c53ee985580443052cf9c9
Danilo <damilhome@gmail.com>
2024-09-02 16:18:37 -0300

    TP3.01 DR4 feita

---
+++ TP3.01 DR4/script.js
@@ -0,0 +1,13 @@
+function soma(a, b) {
+    return a + b;
+}
+function subtracao(a, b) {
+    return a - b;
+}
+function multiplicacao(a, b) {
+    return a * b;
+}
+
+alert(soma(5, 3)); // Deve exibir 8
+alert(subtracao(5, 3)); // Deve exibir 2
+alert(multiplicacao(5, 3)); // Deve exibir 15
